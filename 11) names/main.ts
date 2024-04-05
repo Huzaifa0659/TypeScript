f6ed624d6700b5b7f36429bd04ddce553d7e9563
Huzaifa Ramzan <huzaifahuz061@gmail.com>
2024-04-05 06:33:06 +0500

    added

---
+++ 11) names/main.ts
@@ -0,0 +1,5 @@
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.\
+
+let names = ["Meer","Shayan","Areeb","Kazim"]
+
+names.forEach(name => console.log(name));
