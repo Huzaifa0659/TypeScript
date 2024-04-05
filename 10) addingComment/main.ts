ca489295d8c51c3a973b92babc8e1e9b9a025ba7
Huzaifa Ramzan <huzaifahuz061@gmail.com>
2024-04-05 06:31:03 +0500

    added

---
+++ 10) addingComment/main.ts
@@ -0,0 +1,28 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+
+console.log("// \n\t 1st Program \n\t");
+
+// Addition
+console.log(4 + 4);
+
+// Subtraction
+console.log(16-8);
+
+// Multiplication
+console.log(4*2);
+
+// Division
+console.log(16/2);
+
+
+console.log("\n\t 2nd Program \n\t");
+
+// Storing A Famous Person's Name In A Variable
+let famous_person = "Quaid-e-Azam"
+
+// Storing A Famous Person's Quote In A Variable
+let message = "A nation can never make progress unless it marches in one formation."
+
+// Printing Famous Person's Name And Quote
+console.log(famous_person, "once said,", message);
