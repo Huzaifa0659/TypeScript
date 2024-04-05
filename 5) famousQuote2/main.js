ad7d56b9c0d2ffdd84a130f74a132b90e91cc780
Huzaifa Ramzan <huzaifahuz061@gmail.com>
2024-04-05 06:26:52 +0500

    added

---
+++ 5) famousQuote2/main.js
@@ -0,0 +1,5 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let famous_person = "Quaid-e-Azam";
+let message = "A nation can never make progress unless it marches in one formation.";
+console.log(famous_person, "once said,", message);
+export {};
