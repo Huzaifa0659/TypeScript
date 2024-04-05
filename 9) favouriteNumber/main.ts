0965b3454ee1297db1c42032cdf122ef195cdd9d
Huzaifa Ramzan <huzaifahuz061@gmail.com>
2024-04-05 06:30:05 +0500

    added

---
+++ 9) favouriteNumber/main.ts
@@ -0,0 +1,5 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favNumber = 71
+let message = (`My Favourite Number Is: ${favNumber}`);
+console.log(message);
